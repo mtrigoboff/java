bbf8ae9be02163a8065cb62dccb71505d0328517
Michael Trigoboff <michael.trigoboff@pcc.edu>
2023-09-15 12:23:48 -0700

    formatted

--- gui_demo/animate/Animate.java
+++ gui_demo/animate/Animate.java
@@ -6,14 +6,14 @@ import mlt.SwingWindow;
 
 public class Animate {
 
-    public static void main(String[] args) {
-	// schedule a job for the event-dispatching thread to open the app
-	javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	    public void run() {
-		SwingWindow wn = new SwingWindow("Animate", 400, 400, new AnimatePanel());
-		wn.setQuitChar(KeyEvent.VK_ESCAPE);
-		wn.setResizable(false);
-	    }
-	});
-    }
+	public static void main(String[] args) {
+		// schedule a job for the event-dispatching thread to open the app
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				SwingWindow wn = new SwingWindow("Animate", 400, 400, new AnimatePanel());
+				wn.setQuitChar(KeyEvent.VK_ESCAPE);
+				wn.setResizable(false);
+			}
+		});
+	}
 }
